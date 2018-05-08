073e1a041956bfee035d5cc27b52edf9d62b747f
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-08 15:00:14 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,10 @@
 numbers = [1, 2, 3]
 
 def square_array(numbers)
-  numbers.collect do |i|
-    i**2
+  squared_array = []
+  numbers.each do |i|
+    squared_array << i**2
   end
+  squared_array
 end
+
