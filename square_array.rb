cf8cb6e4c79bc92486fc482c39228ff3529986be
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-08 14:37:47 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,8 @@
 numbers = [1, 2, 3]
 
 def square_array(numbers)
-   numbers.each do |i|
-     i**2
-     square_array(numbers) = squared_array(numbers)
+   numbers.each { |number| number**2 }
+   puts squared_array.ary
   end
   squared_array(numbers)
 end
